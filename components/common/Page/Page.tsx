b226ef5506f9576d1775e426ec83b9748e70cc1d
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-12-21 22:02:42 +0100

    add Page component

---
+++ components/common/Page/Page.tsx
@@ -0,0 +1,18 @@
+import { ComponentType, FC } from 'react';
+import styles from './Page.module.scss';
+import { Footer as DefaultFooter } from '@components/Footer';
+
+interface IPageProps {
+  Header?: ComponentType;
+  Footer?: ComponentType;
+}
+
+export const Page: FC<IPageProps> = ({ children, Header, Footer = <DefaultFooter /> }) => {
+  return (
+    <div className={styles.pageWrapper}>
+      {Header}
+      <main>{children}</main>
+      {Footer}
+    </div>
+  );
+};
