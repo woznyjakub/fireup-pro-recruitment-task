b81f458dd9c93062b069f46427f17f9e15651995
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-12-21 21:10:21 +0100

    add footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer: React.FC = () => {
+  return (
+    <footer className="mt-auto">
+      <div className="container">
+        <p className="text-center sr-only">footer text</p>
+      </div>
+    </footer>
+  );
+};
