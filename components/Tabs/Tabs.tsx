a6c982ed20e26870b26ac15d54e05534c0fe304b
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-12-29 23:53:36 +0100

    add Tabs component placeholder

---
+++ components/Tabs/Tabs.tsx
@@ -0,0 +1,48 @@
+const content = {
+  tabs: [
+    {
+      label: 'BOX NAME 1',
+      labelShort: '1',
+      content: {
+        title: 'Text from box name 1',
+        paragraphs: [
+          '111 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
+        ],
+      },
+    },
+    {
+      label: 'BOX NAME 2',
+      labelShort: '2',
+      content: {
+        title: 'Text from box name 2',
+        paragraphs: [
+          '222 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
+        ],
+      },
+    },
+    {
+      label: 'BOX NAME 3',
+      labelShort: '3',
+      content: {
+        title: 'Text from box name 3',
+        paragraphs: [
+          '333 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
+        ],
+      },
+    },
+    {
+      label: 'BOX NAME 4',
+      labelShort: '4',
+      content: {
+        title: 'Text from box name 4',
+        paragraphs: [
+          '444 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
+        ],
+      },
+    },
+  ],
+};
+
+export const Tabs: React.FC = () => {
+  return <div style={{ height: '60vh', minHeight: 600 }}></div>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Page } from '@components/common/Page';
 import { MainHeader } from '@components/MainHeader';
+import { Tabs } from '@components/Tabs';
 import { Section } from '@components/common/Section';
 import { Gallery } from '@components/Gallery/';
 import { FeaturedTiles } from '@components/FeaturedTiles';
@@ -8,6 +9,7 @@ import { FeaturedTiles } from '@components/FeaturedTiles';
 const Home: React.FC = () => {
   return (
     <Page Header={MainHeader}>
+      <Tabs />
       <Section bgImageUrl="/assets/images/abstract-bg-1.jpg">
         <Gallery className="pb-3 pb-md-4 pb-lg-5" />
         <FeaturedTiles />
