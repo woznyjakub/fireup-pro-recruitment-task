5117f41347e7b978c17226ca9c6c00350d8974cf
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-12-30 00:46:48 +0100

    handle <title> on main page

--- components/common/Page/Page.tsx
+++ components/common/Page/Page.tsx
@@ -1,15 +1,20 @@
 import { ComponentType, FC } from 'react';
+import Head from 'next/head';
 import styles from './Page.module.scss';
 import { Footer as DefaultFooter } from '@components/Footer';
 
 interface IPageProps {
   Header?: ComponentType;
   Footer?: ComponentType;
+  title?: string;
 }
 
-export const Page: FC<IPageProps> = ({ children, Header, Footer = DefaultFooter }) => {
+export const Page: FC<IPageProps> = ({ children, Header, Footer = DefaultFooter, title }) => {
   return (
     <div className={styles.pageWrapper}>
+      <Head>
+        <title>Template{title && ` — ${title}`}</title>
+      </Head>
       {Header && <Header />}
       <main>{children}</main>
       <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { Page } from '@components/common/Page';
 import { MainHeader } from '@components/MainHeader';
 import { Tabs } from '@components/Tabs';
@@ -8,7 +7,7 @@ import { FeaturedTiles } from '@components/FeaturedTiles';
 
 const Home: React.FC = () => {
   return (
-    <Page Header={MainHeader}>
+    <Page Header={MainHeader} title="Main page">
       <Tabs />
       <Section bgImageUrl="/assets/images/abstract-bg-1.jpg">
         <Gallery className="pb-3 pb-md-4 pb-lg-5" />
