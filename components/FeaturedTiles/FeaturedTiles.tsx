008cb74d85fb606f347cbfae99ebdf245a3b14fa
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-12-29 23:49:06 +0100

    add FeaturedTiles component

---
+++ components/FeaturedTiles/FeaturedTiles.tsx
@@ -0,0 +1,122 @@
+import styles from './FeaturedTiles.module.scss';
+import { Picture } from '@utils/Picture';
+import { breakpoints } from '@utils/breakpoints';
+import { PictureElement } from '@entities/picture';
+
+const content = {
+  photoTiles: [
+    {
+      imageUrls: {
+        phone: '/assets/images/eiffel-tower-phone.jpg',
+        tablet: '/assets/images/eiffel-tower-tablet.jpg',
+        desktop: '/assets/images/eiffel-tower-desktop.jpg',
+      },
+      altText: 'Eiffel Tower',
+    },
+    {
+      imageUrls: {
+        phone: '/assets/images/statue-of-liberty-phone.jpg',
+        tablet: '/assets/images/statue-of-liberty-tablet.jpg',
+        desktop: '/assets/images/statue-of-liberty-desktop.jpg',
+      },
+      altText: 'Statue of Liberty',
+    },
+  ],
+  textTiles: [
+    {
+      title: 'Title',
+      titleIconUrl: '/assets/images/edit.png',
+      listContent: [
+        {
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.',
+        },
+        {
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.',
+        },
+        {
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.',
+        },
+        {
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.',
+        },
+        {
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.',
+        },
+        {
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.',
+        },
+      ],
+    },
+  ],
+};
+
+export const FeaturedTiles: React.FC = () => {
+  const { photoTiles, textTiles } = content;
+  return (
+    <div className="row">
+      {photoTiles &&
+        photoTiles.map(({ imageUrls, altText }, i) => {
+          const images: PictureElement[] = [
+            {
+              breakpoint: breakpoints.xs,
+              url: imageUrls.phone,
+            },
+            {
+              breakpoint: breakpoints.md,
+              url: imageUrls.tablet,
+            },
+            {
+              breakpoint: breakpoints.lg,
+              url: imageUrls.desktop,
+              altText,
+            },
+          ];
+
+          return (
+            <div className="col-md-12 col-lg-7 mb-2 mb-md-4 mb-lg-0">
+              <figure className={[styles.imageWrapper, i % 2 !== 0 ? styles.odd : styles.even].join(' ')}>
+                <Picture images={images} className={[styles.image, 'img-stretched'].join(' ')} />
+              </figure>
+            </div>
+          );
+        })}
+
+      {textTiles &&
+        textTiles.map(({ titleIconUrl, title, listContent }) => {
+          return (
+            <div className="col-lg-10 d-flex pt-2 pt-md-0">
+              <div className="bg-white">
+                <div className={styles.titleWrapper}>
+                  <h3 className={styles.title}>
+                    <img src={titleIconUrl} alt={title} className={styles.icon} />
+                    {title}
+                  </h3>
+                </div>
+                {listContent && (
+                  <div className={styles.contentWrapper}>
+                    <ul className={styles.list}>
+                      {listContent.map(({ text }) => {
+                        return (
+                          <li className={styles.listItem}>
+                            <div className={styles.listItemContent}>
+                              <p>{text}</p>
+                            </div>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import { Page } from '@components/common/Page';
 import { MainHeader } from '@components/MainHeader';
 import { Section } from '@components/common/Section';
 import { Gallery } from '@components/Gallery/';
+import { FeaturedTiles } from '@components/FeaturedTiles';
 
 const Home: React.FC = () => {
   return (
     <Page Header={MainHeader}>
       <Section bgImageUrl="/assets/images/abstract-bg-1.jpg">
         <Gallery className="pb-3 pb-md-4 pb-lg-5" />
+        <FeaturedTiles />
       </Section>
     </Page>
   );
