1a588de9f9b728d2e7f688d4228146a6feb0a3a0
Jakub Woźny <jakub_wozny_01@wp.pl>
2021-01-03 00:06:51 +0100

    Section component - handle passing global css classes via props

--- components/common/Section/Section.tsx
+++ components/common/Section/Section.tsx
@@ -1,17 +1,21 @@
 import styles from './Section.module.scss';
 
 interface ISectionProps {
-  tagName?: string;
+  className?: string;
   bgImageUrl?: string;
+  tagName?: string;
 }
 
-export const Section: React.FC<ISectionProps> = ({ children, tagName = 'section', bgImageUrl }) => {
+export const Section: React.FC<ISectionProps> = ({ children, className, tagName = 'section', bgImageUrl }) => {
   // react requires pascal case here
   // @todo: check why ts underlines the `Wrapper` in jsx when it is provided as string (it works well, does not crash)
   const Wrapper = tagName as any;
 
   return (
-    <Wrapper className={`${styles.section} py-3 py-md-4 py-lg-5`} style={{ backgroundImage: `url('${bgImageUrl}')` }}>
+    <Wrapper
+      className={[styles.section, 'py-3', 'py-md-4', 'py-lg-5', className || ''].join(' ')}
+      style={{ backgroundImage: bgImageUrl && `url('${bgImageUrl}')` }}
+    >
       <div className="container">{children}</div>
     </Wrapper>
   );
