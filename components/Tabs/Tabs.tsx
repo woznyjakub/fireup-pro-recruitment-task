4c400e089aa02c90d83bf17332845fdda288ccda
Jakub Woźny <jakub_wozny_01@wp.pl>
2021-01-03 21:24:39 +0100

    Tabs component - get data from /data dir & add data interface

--- components/Tabs/Tabs.tsx
+++ components/Tabs/Tabs.tsx
@@ -5,56 +5,21 @@ import { useWindowSize } from '@hooks/useWindowSize';
 import { Breakpoint } from '@utils/Breakpoint';
 import styles from './Tabs.module.scss';
 
-const content = {
-  tabs: [
-    {
-      label: 'BOX NAME 1',
-      labelShort: '1',
-      buttonColor: 'lightblue',
+interface TabsProps {
+  content: {
+    tabs: Array<{
+      label: string;
+      labelShort: string;
+      buttonColor: string;
       tabContent: {
-        title: 'Text from box name 1',
-        paragraphs: [
-          '111 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
-        ],
-      },
-    },
-    {
-      label: 'BOX NAME 2',
-      labelShort: '2',
-      buttonColor: 'red',
-      tabContent: {
-        title: 'Text from box name 2',
-        paragraphs: [
-          '222 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
-        ],
-      },
-    },
-    {
-      label: 'BOX NAME 3',
-      labelShort: '3',
-      buttonColor: 'yellow',
-      tabContent: {
-        title: 'Text from box name 3',
-        paragraphs: [
-          '333 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
-        ],
-      },
-    },
-    {
-      label: 'BOX NAME 4',
-      labelShort: '4',
-      buttonColor: 'green',
-      tabContent: {
-        title: 'Text from box name 4',
-        paragraphs: [
-          '444 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.',
-        ],
-      },
-    },
-  ],
-};
+        title: string;
+        paragraphs: string[];
+      };
+    }>;
+  };
+}
 
-export const Tabs: FC = () => {
+export const Tabs: FC<TabsProps> = ({ content }) => {
   const { width: windowWidth } = useWindowSize(true);
   const [activeSlideIndex, setActiveSliceIndex] = useState<number>(0);
   const [tabContentSlider, setTabContentSlider] = useState<SwiperType>(undefined);
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
   return (
     <Page Header={MainHeader} title="Main page">
       <Section className="bg-grey-2">
-        <Tabs />
+        <Tabs content={content.tabs} />
       </Section>
       <Section bgImageUrl="/assets/images/abstract-bg-1.jpg">
         <Gallery className="pb-3 pb-md-4 pb-lg-5" content={content.gallery} />
